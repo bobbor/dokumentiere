d1887ef87f10713a0daeb7d9590a183a3aca17a5
Philipp Paul <me@philipp-paul.com>
2012-07-13 14:12:38 +0200

    templating en masse

--- src/tmpl.js
+++ src/tmpl.js
@@ -6,28 +6,32 @@ var fc = require('./foldercopy');
 
 path.sep = path.sep || require('os').platform().indexOf('win') !== -1 ? '\\' : '/';
 
+
 var tmpl = {
 	generate: function( obj, out ) {
+		this.out = out;
 		var index = [];
 		for( var prop in obj) {
 			if ( obj.hasOwnProperty( prop ) ) {
 				index = index.concat( obj[prop] );
-				this.items( obj[prop], out );
 			}
 		}
+		this.index(index);
+		
+		for( var prop in obj) {
+			if ( obj.hasOwnProperty( prop ) ) {
+				this.items( obj[prop], index);
+			}
+		}
+		
 		fc.recursiveCopy(['css', 'img'], ['templates','public'], out);
 	},
-	items: function( arr, out ) {
+	index: function(arr) {
+		this.render('index', { index: arr }, this.writeHTML);
+	},
+	items: function( arr, index) {
 		for( var i = 0, len = arr.length; i < len; i++) {
-			this.render('module', arr[i], function(html, name) {
-				fs.writeFile(cwd+path.sep+out+path.sep+name+'.html', html, 'utf8', function(err) {
-					if(err) {
-						console.log('could not document '+name+'.html');
-						return;
-					}
-					console.log('documented '+name+'.html');
-				});
-			});
+			this.render('module', { index: index, item: arr[i]}, this.writeHTML);
 		}
 	},
 	render: function(tmplName, data, callback) {
@@ -35,13 +39,20 @@ var tmpl = {
 		fs.readFile(name, 'utf8', function(err, content) {
 			if(err) { throw err; }
 			var fn = jade.compile(content, {
-				filename: name
+				filename: name,
+				self: true
 			});
-			callback(fn(data), data.name);
+			callback(fn(data), (data.item && data.item.name) || 'index');
 		});
 	},
-	writeHTML: function( html ) {
-
+	writeHTML: function(html, name) {
+		fs.writeFile(cwd+path.sep+this.out+path.sep+name+'.html', html, 'utf8', function(err) {
+			if(err) {
+				console.log('could not document '+name+'.html');
+				return;
+			}
+			console.log('documented '+name+'.html');
+		});
 	}
 };
 
